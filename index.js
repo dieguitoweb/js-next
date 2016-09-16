ce8d65126fff267c7d55b99570e7122b17548afc
Diego Costantino <diegocstn@gmail.com>
2016-09-16 08:28:42 +0200

    substitute return with process.exit

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const chalk = require('chalk');
 const log = require('./lib/log');
 const fsHelpers = require('./lib/fs-helpers');
+
   
 if (!process.argv[2]) {
   log.err('app name is required');
   log.text('usage: js-next app_name');
-  return;
+  process.exit(1);
 }
 
 const appName = process.argv[2];
 if (fsHelpers.dirExists(appName)) {
   log.err('app "' + appName  + '" already exists!');
+  process.exit(1);
 }
+
+
 
 
